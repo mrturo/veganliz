e86b40359c6f894277f855e0323e605494264d05
Arturo <arturo.amb89@gmail.com>
2020-02-16 21:26:03 -0300

    VeganLiz Theme

---
+++ wp-content/themes/veganliz/functions.php
@@ -0,0 +1,25 @@
+<?php 
+
+function init_template(){
+
+    add_theme_support('post-thumbnails');
+    add_theme_support( 'title-tag');
+
+    register_nav_menus(
+        array(
+            'top_menu' => 'Menú Principal'
+        )
+    );
+
+}
+
+add_action('after_setup_theme','init_template');
+
+
+function assets(){
+    wp_register_style('bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', '', '4.4.1','all');
+    wp_register_style('montserrat', 'https://fonts.googleapis.com/css?family=Montserrat&display=swap','','1.0', 'all');
+    wp_enqueue_style('estilos', get_stylesheet_uri(), array('bootstrap','montserrat'),'1.0', 'all');
+}
+
+add_action('wp_enqueue_scripts','assets');
